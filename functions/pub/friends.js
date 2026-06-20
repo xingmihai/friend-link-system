02e8a65f14499a81459b6810a882f5ef6282cd7c
zlm6666 <548541786@qq.com>
2026-06-20 01:39:04 +0000

    fix: /pub/friends用内部fetch转发

--- functions/pub/friends.js
+++ functions/pub/friends.js
@@ -1,2 +1,11 @@
-// 兼容 Qexo 友链路径
-export { onRequestGet, onRequestPost, onRequestOptions } from '../api/links-qexo.js';
+// functions/pub/friends.js
+// 中转 /pub/friends 到 /api/links-qexo
+
+export async function onRequestGet(ctx) {
+  const url = new URL(ctx.request.url);
+  url.pathname = '/api/links-qexo';
+  return fetch(url.toString());
+}
+
+export const onRequestPost = onRequestGet;
+export const onRequestOptions = onRequestGet;
